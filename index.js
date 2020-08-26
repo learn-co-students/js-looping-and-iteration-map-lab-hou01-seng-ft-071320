82953c59bae20b4eb70bbbc1b579a5898b6f8f04
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-08-26 09:47:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file.
+
+// const array = ['DANIEL', 'VICTORIA']
+// const array = ['Daniel Cole', 'Victoria Gordon']
+
+const lowerCaseDrivers = function(array) {
+    return array.map(function(driver){
+        return driver.toLowerCase()
+    });
+};
+
+// console.log(lowerCaseDrivers(array))
+
+const nameToAttributes = function(array) {
+    return array.map(function(driver){
+        let splitNames = driver.split(' ');
+        let first = splitNames[0];
+        let last = splitNames[1];
+        return { firstName: first, lastName: last }
+    });
+};
+
+// console.log(nameToAttributes(array))
+
+const attributesToPhrase = function(array) {
+    return array.map(function(driver){
+        return `${driver.name} is from ${driver.hometown}`
+    });
+};
